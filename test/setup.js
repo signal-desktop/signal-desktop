cb7700d251848e7f4fb0fcb213db7a2f8d03b37a
John <john@haley.io>
2017-05-05 23:34:18 -0700

    Initial commit from boilerplate

---
+++ test/setup.js
@@ -0,0 +1,17 @@
+import { jsdom } from 'jsdom';
+
+global.document = jsdom('<!doctype html><html><body></body></html>');
+global.window = document.defaultView;
+global.navigator = global.window.navigator;
+window.sessionStorage = {
+  getItem(key) {
+    return this[key];
+  },
+  setItem(key, value) {
+    this[key] = value;
+  },
+  removeItem(key) {
+    this[key] = undefined;
+  },
+};
+window.localStorage = window.sessionStorage;
